4feda2426a32c98516408c617d952725f71698f3
lianjie chen <lchen@amarsoft.com>
2017-02-20 11:36:25 +0800

    first commit

---
+++ sample/index.jsx
@@ -0,0 +1,10 @@
+var ReactDom =require('react-dom')
+var React =require('react')
+var log=require('../util/log1')
+module.exports=function (){
+    log('start render')
+    ReactDom.render(
+        <h1>Hello333333,112227 world!</h1>,
+        document.getElementById('root')
+    );
+}
